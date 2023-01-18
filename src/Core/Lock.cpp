9c0f1c81f5663eb4cc8250248d2ff7471aef6f03
CULRRY <rkaghwns11@gmail.com>
2023-01-19 06:54:24 +0900

    [feat] rw lock 완성

--- src/Core/Lock.cpp
+++ src/Core/Lock.cpp
@@ -3,8 +3,8 @@
 
 void Lock::WriteLock(const char* name)
 {
-	const uint32 lockTreadID = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
-	if (lockTreadID == LThreadID)
+	const uint32 lockTreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
+	if (lockTreadId == LThreadID)
 	{
 		_writeCount++;
 		return;
@@ -25,7 +25,9 @@ void Lock::WriteLock(const char* name)
 		}
 
 		if (::GetTickCount64() - beginTick >= ACQUIRE_TIMEOUT_TICK)
+		{
 			CRASH("Lock Timeout");
+		}
 
 		this_thread::yield();
 	}
@@ -33,12 +35,52 @@ void Lock::WriteLock(const char* name)
 
 void Lock::WriteUnlock(const char* name)
 {
+	if ((_lockFlag.load() & READ_COUNT_MASK) != 0)
+	{
+		CRASH("Invalid unlock mask");
+	}
+
+	const int32 lockCount = --_writeCount;
+	if (lockCount == 0)
+	{
+		_lockFlag.store(EMPTY_FLAG);
+	}
 }
 
 void Lock::ReadLock(const char* name)
 {
+	const uint32 lockThreadId = (_lockFlag.load() & WRITE_THREAD_MASK) >> 16;
+	if (lockThreadId == LThreadID)
+	{
+		_lockFlag.fetch_add(1);
+		return;
+	}
+
+	const uint64 beginTick = ::GetTickCount64();
+	while (true)
+	{
+		for (uint32 spinCount = 0; spinCount < MAX_SPIN_COUNT; spinCount++)
+		{
+			uint32 expected = _lockFlag & READ_COUNT_MASK;
+			if (_lockFlag.compare_exchange_strong(expected, expected + 1));
+			{
+				return;
+			}
+		}
+
+		if (::GetTickCount64() - beginTick >= ACQUIRE_TIMEOUT_TICK)
+		{
+			CRASH("Lock Timeout");
+		}
+
+		this_thread::yield();
+	}
 }
 
 void Lock::ReadUnlock(const char* name)
 {
+	if ((_lockFlag.fetch_sub(1) & READ_COUNT_MASK) == 0)
+	{
+		CRASH("Multiple Unlock");
+	}
 }
--- src/Core/Lock.h
+++ src/Core/Lock.h
@@ -13,14 +13,12 @@
 
 class Lock
 {
-	enum : uint32
-	{
-		ACQUIRE_TIMEOUT_TICK = 10000,
-		MAX_SPIN_COUNT = 5000,
-		WRITE_THREAD_MASK = 0xFFFF'0000,
-		READ_COUNT_MASK = 0x0000'FFFF,
-		EMPTY_FLAG = 0x0000'0000,
-	};
+	static constexpr uint32
+		ACQUIRE_TIMEOUT_TICK	= 10000,
+		MAX_SPIN_COUNT			= 5000,
+		WRITE_THREAD_MASK		= 0xFFFF'0000,
+		READ_COUNT_MASK			= 0x0000'FFFF,
+		EMPTY_FLAG				= 0x0000'0000;
 
 public:
 	void			WriteLock(const char* name);
